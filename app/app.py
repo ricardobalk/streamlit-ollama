f2c31310643fd5740c5086bd5db11ea2ace41dd7
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2024-04-25 23:09:19 +0200

    * feat(app.py): refactor Streamlit session initialization and model selection * feat(app.py): add function to load models from JSON file * feat(models.json): add new file to store model data

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import datetime
-import streamlit as st
+import json
 import ollama
+import streamlit as st
 
 ollama_client = ollama.Client(host='http://ollama:11434')
          
@@ -15,25 +16,45 @@ st.set_page_config(
     layout="wide",
 )
 
-# Initialise the chat if not in Streamlit session's state.
-if 'chat_messages' not in st.session_state:
-  st.session_state['chat_messages'] = [
-      {"role": "system", "content": "You're an AI assistant that helps users with a multitude of tasks."}
-  ]
+# Initializes Streamlit session state variables
+def initialize_streamlit_session():
+  if 'chat_messages' not in st.session_state:
+    st.session_state['chat_messages'] = [
+        {"role": "system", "content": "You're an AI assistant that helps users with a multitude of tasks."}
+    ]
 
-if 'model' not in st.session_state:
-  st.session_state['model'] = 'llama2'
+  if 'model' not in st.session_state:
+    st.session_state['model'] = 'llama2'
+
+# Loads available models from JSON file
+def load_models():
+    with open('./data/models.json', 'r') as file:
+        data = json.load(file)
+    return data['models']
+
+# Model selection module to be used within the Streamlit App layout.
+def sl_module_model_selection():
+    st.subheader("Model")
+    models = load_models()
+    # Creating a dropdown box with names and descriptions stored separately
+    model_options = {model['id']: (model['name'], model['description']) for model in models}
+    model_id = st.selectbox(
+        "Choose a model",
+        options=list(model_options.keys()),
+        format_func=lambda x: model_options[x][0]  # Display only model names in the dropdown
+    )
+    if model_id:
+        st.session_state['model'] = model_id
+        st.write(model_options[model_id][1]) # Accessing the description of the model
+
+
+### Streamlit app
+
+initialize_streamlit_session()
 
 with st.sidebar:
-  st.header("Preferences")
-
-  st.subheader("Model")
-  model = st.radio(
-    "Choose a model",
-    ("llama2", "llama3")
-  )
-  if model:
-    st.session_state['model'] = model
+    st.header("Preferences")
+    sl_module_model_selection()
 
 st.header(f"{package_data['name']}")
 
